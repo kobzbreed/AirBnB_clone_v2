888bd499a0204b72cb435c9c55bb92ac40e4c02e
Okwor Okechukwu Joshua <joshuaokwor565@icloud.com>
2023-04-06 16:57:01 -0700

    tast 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-'''Fabric script that generates a .tgz archive from the contents of the web_static'''
-
-from fabric.api import local
+""" Function that compress a folder """
 from datetime import datetime
+from fabric.api import local
+import os
 
-from fabric.decorators import runs_once
 
-@runs_once
 def do_pack():
-    '''generates .tgz archive from the contents of the web_static folder'''
-    local("mkdir -p versions")
-    path = ("versions/web_static_{}.tgz"
-            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    result = local("tar -cvzf {} web_static"
-                   .format(path))
-
-    if result.failed:
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        return archive_path
+    except:
         return None
-    return path
