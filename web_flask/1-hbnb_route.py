270facbd281e8a0c08113d37e8115530f5eb39d1
kobzbreed <jitekobimowe10@gmail.com>
2023-04-25 09:28:55 -0700

    add 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-write a script that starts a flask web application 
+write a script that starts a flask web application
 web application must listening on 0.0.0.0, port 5000
 """
 from flask import Flask
