d0c0f6d1ced059253beffbf49377f85c627d296d
kobzbreed <jitekobimowe10@gmail.com>
2023-04-26 05:32:11 -0700

    8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+The application must be listening on 0.0.0.0, port 5000
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(exc):
+    """
+    Close database
+    """
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def displays_states():
+    """
+    displays state HTML page
+    """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
